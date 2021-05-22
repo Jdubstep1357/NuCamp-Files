dedde5bcf07bf01fa27aa8c6f14d323d8fa94f01
John Lyons <jwlyons2015@gmail.com>
2021-05-22 12:25:14 -0400

    Git jumbotron button Error

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,29 @@
 $(function() {
     $(".carousel").carousel( { interval: 2000 } );
     $("#carouselButton").click(function() {
+        console.log("Hi");
         if($("#carouselButton").children("i").hasClass("fa-pause")) {
+            console.log("Hi 2");
             $(".carousel").carousel("pause");
             $("#carouselButton").children("i").removeClass("fa-pause");
             $("#carouselButton").children("i").addClass("fa-play");
         } else {
+            console.log("Hi 3")
             $(".carousel").carousel("cycle");
             $("#carouselButton").children("i").removeClass("fa-play");
             $("#carouselButton").children("i").addClass("fa-pause");
         }
+    });
+});
+
+$(document).ready(function(){
+    $("#reserveButton").click(function(){
+        $("#reserveModal").modal('show');
+    });
+});
+
+$(document).ready(function(){
+    $("#loginButton").click(function(){
+        $("#loginModal").modal('show');
     });
 });
